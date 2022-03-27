eb3687f63ef9766c248bd675d092c99df622dc8d
dnkc <dnkcdesign@gmail.com>
2022-03-27 14:38:52 -0400

    make it better

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/users", users);
 app.use("/api/profile", profile);
