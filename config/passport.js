40ab31597fab51ca2915a71974b49e694664dbdb
dnkc <dnkcdesign@gmail.com>
2022-03-27 14:33:33 -0400

    make it better

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ const keys = require("../config/keys");
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = "secretKey";
+opts.secretOrKey = keys.secretKey;
 
 module.exports = (passport) => {
   passport.use(
