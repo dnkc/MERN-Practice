57c0169dc611c6f51b80a3fa9af99d6970c52e4f
dnkc <dnkcdesign@gmail.com>
2022-03-27 15:08:16 -0400

    make it better

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.append("Access-Control-Allow-Origin", [
-    "https://dk-social-connector.herokuapp.com/",
-  ]);
+  res.append("Access-Control-Allow-Origin", ["*"]);
   res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.append("Access-Control-Allow-Headers", "Content-Type");
   next();
