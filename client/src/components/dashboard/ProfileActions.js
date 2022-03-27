515d7da6c480ed30124278d021b6b2ac756e0b9d
dnkc <dnkcdesign@gmail.com>
2022-03-27 18:20:04 -0400

    make it better

--- client/src/components/dashboard/ProfileActions.js
+++ client/src/components/dashboard/ProfileActions.js
@@ -11,10 +11,10 @@ const ProfileActions = () => {
         <i className="fab fa-black-tie text-info mr-1"></i>
         Add Experience
       </Link>
-      <Link to="/add-education" className="btn btn-light">
+      {/* <Link to="/add-education" className="btn btn-light">
         <i className="fas fa-graduation-cap text-info mr-1"></i>
         Add Education
-      </Link>
+      </Link> */}
     </div>
   );
 };
