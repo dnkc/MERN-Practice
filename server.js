acd790d8efb925c5770f06614b0ad6e245e27e65
dnkc <dnkcdesign@gmail.com>
2022-03-27 14:46:58 -0400

    make it better

--- server.js
+++ server.js
@@ -6,12 +6,25 @@ const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 const passport = require("passport");
+const csp = require("express-csp-header");
 
 const app = express();
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(
+  csp({
+    policies: {
+      "default-src": [csp.NONE],
+      "img-src": [csp.SELF],
+    },
+  })
+);
+
+// HTTP response header will be defined as:
+// "Content-Security-Policy: default-src 'none'; img-src 'self';"
 
 // DB Config
 const MONGO_DB = require("./config/keys").mongoURI;
